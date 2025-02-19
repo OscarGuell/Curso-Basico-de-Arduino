dfee20567f2211cce35a51cf49aab3c1e53e0d2b
OscarGuell <oscar.guellzavaleta@ucr.ac.cr>
2025-02-19 16:49:40 -0600

    New 'giro_antihorario'

--- Creaciones/rubik.cpp
+++ Creaciones/rubik.cpp
@@ -92,6 +92,97 @@ void giro_horario(int* grid, int* p, int eje) {
 	}
 }
 
+void giro_antihorario(int* grid, int* p, int eje) {
+
+	// Parte 1: Caras laterales.
+	int aux[3];
+
+	// Caso A: Cara roja (0).
+	if (eje == 0) {
+		// Respaldar los valores de la última cara (4) en un array auxiliar.
+		for (int i = 0; i < 3; i++) {
+			aux[i] = grid[4 * 9 + i];
+		}
+		// Sobreescribir la cara actual con los valores de la cara anterior.
+		for (int c = 4; c > 1; c--) {
+			for (int i = 0; i < 3; i++) {
+				grid[c * 9 + i] = grid[(c - 1) * 9 + i];
+			}
+		}
+		// Asignar los valores guardados a la primera cara (1).
+		for (int i = 0; i < 3; i++) {
+			grid[9 + i] = aux[i];
+		}
+
+	// Caso B: Cara naranja (5).
+	} else if (eje == 5) {
+		// Respaldar los valores de la primera cara (1) en un array auxiliar.
+		for (int i = 0; i < 3; i++) {
+			aux[i] = grid[9 + i];
+		}
+		// Sobreescribir la cara actual con los valores de la siguiente cara.
+		for (int c = 1; c < 4; c++) {
+			for (int i = 0; i < 3; i++) {
+				grid[c * 9 + i] = grid[(c + 1) * 9 + i];
+			}
+		}
+		// Asignar los valores guardados a la última cara (4).
+		for (int i = 0; i < 3; i++) {
+			grid[4 * 9 + i] = aux[i];
+		}
+
+	// Caso C: Azul (1), Blanco (2), Verde(3) o Amarillo (4).
+	} else {
+
+		int c1 = (eje + 2) % 4 + 1;
+		int c2 = eje % 4 + 1;
+
+		// Paso 1: Respaldar los valores de la cara final (C2) en 'aux'.
+		for (int i = 0; i < 3; i++) {
+			aux[i] = grid[c2 * 9 + p[(eje - 1) * 12 + i + 3 * 3]];
+		}
+
+		// Paso 2: Actualizar 'C2'.
+		for (int i = 0; i < 3; i++) {
+			// [c2 <- 5].
+			grid[c2 * 9 + p[(eje - 1) * 12 + i + 3 * 3]] = grid[5 * 9 + p[(eje - 1) * 12 + ((eje == 1 || eje == 4) ? (2 - i) : i) + 3 * 2]];
+		}
+
+		// Paso 3: Actualizar '5'.
+		for (int i = 0; i < 3; i++) {
+			// [5 <- c1].
+			grid[5 * 9 + p[(eje - 1) * 12 + ((eje == 3 || eje == 4) ? (2 - i) : i) + 3 * 2]] = grid[c1 * 9 + p[(eje - 1) * 12 + 3 * 1 + i]];
+		}
+
+		// Paso 4: Actualizar 'C1'.
+		for (int i = 0; i < 3; i++) {
+			// [c1 <- 0].
+			grid[c1 * 9 + p[(eje - 1) * 12 + 3 * 1 + i]] = grid[p[(eje - 1) * 12 + i]];
+		}
+
+		// Paso 5: Usar 'aux' para actualizar la cara inicial (0).
+		for (int i = 0; i < 3; i++) {
+			// [0 <- aux].
+			grid[p[(eje - 1) * 12 + i]] = aux[i];
+		}
+	}
+
+	// Paso 2: Rotación de la cara seleccionada (A[j][2-i] -> A[i][j]).
+
+	// Respaldar los valores originales de la cara a rotar en un array temporal.
+	int temp[9];
+	for (int i = 0; i < 9; i++) {
+		temp[i] = grid[eje * 9 + i];
+	}
+
+	// Aplicar la rotación A[j][2-i] -> A[i][j].
+	for (int j = 0; j < 3; j++) {
+		for (int i = 0; i < 3; i++) {
+			grid[eje * 9 + i * 3 + j] = temp[j * 3 + (2 - i)];
+		}
+	}
+}
+
 void print_grid(int* grid) {
 	for (int c = 0; c < 6; c++) {
 		std::cout << " --- " << c << " ---" << std::endl;
